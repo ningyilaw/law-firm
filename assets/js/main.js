b40c06fff8211c68f063cbf54d01259dd4c06a12
huang zhihao <71325042+huangzhihao778@users.noreply.github.com>
2025-07-05 14:21:21 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+// 图片懒加载
+document.addEventListener("DOMContentLoaded", function() {
+  const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImage.classList.add("loaded");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
